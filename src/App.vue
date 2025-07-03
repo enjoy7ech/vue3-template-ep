d8af310b1bf15adc3760b3e63a04eb5ad890fed4
zhenxiang <zxdong@agioe.com>
2025-07-03 16:52:15 +0800

    refactor: remove unused layout import and provide statement in App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,8 @@
 <script setup>
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import en from 'element-plus/dist/locale/en.mjs'
-import Standard from './layouts/Standard.vue'
 
 const locale = computed(() => (document.documentElement.lang === 'zh-CN' ? zhCn : en))
-
-provide('layout', Standard)
 </script>
 
 <style scoped></style>
